09a4eeda3c7ae6817dde259489597311665e1da2
jBalasis <cs2180008@di.uoa.gr>
2021-03-02 16:57:38 +0200

    table creation script changes

--- start.sh
+++ start.sh
@@ -23,8 +23,8 @@ cd /recdb/bin/ && \
      LC_CTYPE = 'C'
      CONNECTION LIMIT = -1;"
 ./psql -U postgres -d recdb -c "CREATE TABLE IF NOT EXISTS recommendationTable (
-   user_email VARCHAR ( 255 ) UNIQUE NOT NULL,
-   service_id VARCHAR ( 255 ) UNIQUE NOT NULL,
+   user_email VARCHAR ( 255 ) NOT NULL,
+   service_id VARCHAR ( 255 ) NOT NULL,
    visits INT NOT NULL,
 
    PRIMARY KEY (user_email, service_id)
